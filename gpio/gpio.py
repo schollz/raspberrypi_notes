998311907fa784f8180ac2e01699c5ee81ca5ad2
Zack <zack.scholl@gmail.com>
2015-06-05 22:32:11 -0400

    Update gpio.py

--- gpio/gpio.py
+++ gpio/gpio.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
-# 1.7 khz
+
 from time import sleep           # Allows us to call the sleep function to slow down our loop
 import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
 
 GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
-INPUT_PIN = 5           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
+INPUT_PIN = 4           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
 GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
 
 # Start a loop that never ends
 j=0
-while True:
-           j = j+1
-           print j
-           if (GPIO.input(INPUT_PIN) == True): # Physically read the pin now
-                    print('3.3')
-           else:
-                    print('0')
+for i in range(100000000):
+           j = GPIO.input(INPUT_PIN)# Physically read the pin now
